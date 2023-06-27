56a8f23d97fe2fae934e21ed1d156de0bb5d4ecb
shmulisarmy <126830519+shmulisarmy@users.noreply.github.com>
2023-06-27 19:32:19 -0400

    Add files via upload

---
+++ rock_papper_sciccors.py
@@ -0,0 +1,34 @@
+import random
+
+#keeps track of wins
+pl, co = 0, 0 
+
+#stops the game when someone wins
+while True:
+    round += 1
+
+    print(f"round {round}!!!\n")
+
+    things = ['rock', 'sciccors','papper']
+    player = input("pick either 'rock', 'papper' or 'sciccors': ")
+    while player not in things:
+        print ("invalid choice, try again!")
+        player = input("pick either 'rock', 'papper' or 'sciccors': ")
+    computer = random.choice(things)
+    print(f"computer picked {computer}")
+    if player == computer:
+        print("its a draw\n")
+    else:
+        for i in range(len(things)):
+            if computer == things[i] and player == things[i-1]:
+                print(f'player wins round {round}\n')
+                pl += 1
+            elif player == things[i] and computer == things[i-1]:
+                print(f'computer wins round {round}\n')
+                co += 1
+    if pl == 3:
+        print(f"player reached 3 first player wins")
+        break
+    elif co == 3:
+        print(f"computer reached 3 first computer wins")
+        break
