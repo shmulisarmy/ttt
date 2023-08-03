5004d5e5b7281447e6d8e97008fc944336d99496
mendel keller <mendelthekeller@alum.mit.edu>
2023-08-02 20:05:20 -0400

    Reformatted as function

--- rock_papper_sciccors.py
+++ rock_papper_sciccors.py
@@ -1,35 +1,42 @@
 import random
 
 #keeps track of wins
-pl, co = 0, 0 
+scores = {
+    'player': 0,
+    'computer': 0
+}
 
 match = 0
 #stops the game when someone wins
+
+def match_result(player, computer):
+    if player == computer:
+        return 'player'
+    else:
+        for i in range(len(things)):
+            if computer == things[i] and player == things[i-1]:
+                return 'player'
+            elif player == things[i] and computer == things[i-1]:
+                return 'computer'
+
 while True:
     match += 1
 
     print(f"match {match}!!!\n")
 
-    things = ['rock', 'scissors','paper']
+    things = ['rock', 'scissors', 'paper']
     player = input("pick either rock, paper or scissors: ")
     while player not in things:
         print ("invalid choice, try again!")
         player = input("pick either 'rock', 'paper' or 'scissors': ")
     computer = random.choice(things)
     print(f"computer picked {computer}")
-    if player == computer:
-        print("its a draw\n")
-    else:
-        for i in range(len(things)):
-            if computer == things[i] and player == things[i-1]:
-                print(f'player wins match {match}\n')
-                pl += 1
-            elif player == things[i] and computer == things[i-1]:
-                print(f'computer wins match {match}\n')
-                co += 1
-    if pl == 3:
+    winner = match_result(player, computer)
+    print(f"{winner} wins match {match}")
+    scores[winner] += 1
+    if scores['player'] == 3:
         print(f"player reached 3 first player wins")
         break
-    elif co == 3:
+    elif scores['computer'] == 3:
         print(f"computer reached 3 first computer wins")
         break
